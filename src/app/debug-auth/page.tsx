3c5b19aa1479759dfb4b870edb97163483048271
adhnous <dhnos@hotmail.com>
2025-10-20 19:58:16 -0400

    wipe repository content

---
+++ src/app/debug-auth/page.tsx
@@ -0,0 +1,8 @@
+export default function DebugAuthPage() {
+  return (
+    <div className="p-8">
+      <h1 className="text-xl font-bold">Debug Auth</h1>
+      <p>Authentication debugging page.</p>
+    </div>
+  );
+}
